74fe46900a9a12d95e5bf5b6b0248281515d7052
PibaMutanda <pibamutanda@gmail.com>
2015-08-09 15:09:03 +0200

    listArticleServlet bug resolved

--- ShopOnlineWeb/src/main/java/be/shop/servlet/ListArticleServlet.java
+++ ShopOnlineWeb/src/main/java/be/shop/servlet/ListArticleServlet.java
@@ -39,7 +39,7 @@ public class ListArticleServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		   String idCateg = request.getParameter("id");
-		   Categorie categ = categorieServices.findById(Long.parseLong(idCateg.trim()));
+		   Categorie categ = categorieServices.findById(Long.parseLong(idCateg));
 	       List<Article> articles = articleServices.findByCateg(categ);
 	       request.setAttribute("articles", articles);
 	       request.getRequestDispatcher("/WEB-INF/views/displayListArticles.jsp").forward(request, response);
